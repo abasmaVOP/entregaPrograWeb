c4bf69bfa2979edfdd4b0b131e757dc90b04b695
abasmaVOP <basma237gamer@gmail.com>
2025-05-24 23:25:46 -0300

    peque avance visual

---
+++ scripts.js
@@ -0,0 +1,132 @@
+const runeData = {
+    precision: {
+        keystones: [
+            { nombre: "Estrategia Ofensiva", img: "https://static.bigbrain.gg/assets/lol/riot_static/15.10.1/img/perk-images/Styles/Precision/PressTheAttack/PressTheAttack.png" },
+            { nombre: "Cadencia Letal", img: "https://static.bigbrain.gg/assets/lol/riot_static/15.10.1/img/small-perk-images/Styles/Precision/LethalTempo/LethalTempoTemp.png" },
+            { nombre: "Sobre la Marcha", img: "https://static.bigbrain.gg/assets/lol/riot_static/15.10.1/img/perk-images/Styles/Precision/FleetFootwork/FleetFootwork.png" },
+            { nombre: "Conquistador", img: "https://static.bigbrain.gg/assets/lol/riot_static/15.10.1/img/perk-images/Styles/Precision/Conqueror/Conqueror.png" }
+        ],
+        slots: [
+            [
+                { nombre: "Absorber Vida", img: "https://static.bigbrain.gg/assets/lol/riot_static/15.10.1/img/small-perk-images/Styles/Precision/AbsorbLife/AbsorbLife.png" },
+                { nombre: "Triunfo", img: "https://static.bigbrain.gg/assets/lol/riot_static/15.10.1/img/small-perk-images/Styles/Precision/Triumph.png" },
+                { nombre: "Concentración Profunda", img: "https://static.bigbrain.gg/assets/lol/riot_static/15.10.1/img/perk-images/Styles/Precision/PresenceOfMind/PresenceOfMind.png" },
+            ],
+            [
+                { nombre: "Leyenda: Celeridad", img: "https://static.bigbrain.gg/assets/lol/riot_static/15.10.1/img/perk-images/Styles/Precision/LegendAlacrity/LegendAlacrity.png" },
+                { nombre: "Leyenda: Aceleración", img: "https://static.bigbrain.gg/assets/lol/riot_static/15.10.1/img/perk-images/Styles/Precision/LegendHaste/LegendHaste.png" },
+                { nombre: "Leyenda: Linaje", img: "https://static.bigbrain.gg/assets/lol/riot_static/15.10.1/img/perk-images/Styles/Precision/LegendBloodline/LegendBloodline.png" }
+            ],
+            [
+                { nombre: "Golpe de Gracia", img: "https://static.bigbrain.gg/assets/lol/riot_static/15.10.1/img/perk-images/Styles/Precision/CoupDeGrace/CoupDeGrace.png" },
+                { nombre: "Corte", img: "https://static.bigbrain.gg/assets/lol/riot_static/15.10.1/img/perk-images/Styles/Precision/CutDown/CutDown.png" },
+                { nombre: "La última Batalla", img: "https://static.bigbrain.gg/assets/lol/riot_static/15.10.1/img/small-perk-images/Styles/Sorcery/LastStand/LastStand.png" } 
+            ]
+        ]
+    },
+    domination: {
+        keystones: [
+            { nombre: "Electrocutar", img: "https://static.bigbrain.gg/assets/lol/riot_static/15.10.1/img/perk-images/Styles/Domination/Electrocute/Electrocute.png" },
+            { nombre: "Cosecha Oscura", img: "https://static.bigbrain.gg/assets/lol/riot_static/15.10.1/img/perk-images/Styles/Domination/DarkHarvest/DarkHarvest.png" },
+            { nombre: "LLuvia de Espadas", img: "https://static.bigbrain.gg/assets/lol/riot_static/15.10.1/img/small-perk-images/Styles/Domination/HailOfBlades/HailOfBlades.png" }
+        ],
+        slots: [
+            [
+                { nombre: "Golpe Bajo", img: "https://static.bigbrain.gg/assets/lol/riot_static/15.10.1/img/perk-images/Styles/Domination/CheapShot/CheapShot.png" },
+                { nombre: "Sabor a Sangre", img: "https://static.bigbrain.gg/assets/lol/riot_static/15.10.1/img/small-perk-images/Styles/Domination/TasteOfBlood/GreenTerror_TasteOfBlood.png" }, 
+                { nombre: "Impacto Súbito", img: "https://static.bigbrain.gg/assets/lol/riot_static/15.10.1/img/perk-images/Styles/Domination/SuddenImpact/SuddenImpact.png" }
+            ],
+            [
+                { nombre: "Sexto Sentido", img: "https://static.bigbrain.gg/assets/lol/riot_static/15.10.1/img/small-perk-images/Styles/Domination/SixthSense/SixthSense.png" },
+                { nombre: "Recuerdos Macabros", img: "https://static.bigbrain.gg/assets/lol/riot_static/15.10.1/img/small-perk-images/Styles/Domination/GrislyMementos/GrislyMementos.png" },
+                { nombre: "Centinela Profundo", img: "https://static.bigbrain.gg/assets/lol/riot_static/15.10.1/img/small-perk-images/Styles/Domination/DeepWard/DeepWard.png" }
+            ],
+            [
+                { nombre: "Cazador de Tesoros", img: "https://static.bigbrain.gg/assets/lol/riot_static/15.10.1/img/perk-images/Styles/Domination/TreasureHunter/TreasureHunter.png" },
+                { nombre: "Cazador Implacable", img: "https://static.bigbrain.gg/assets/lol/riot_static/15.10.1/img/perk-images/Styles/Domination/RelentlessHunter/RelentlessHunter.png" },
+                { nombre: "Cazador Definitivo", img: "https://static.bigbrain.gg/assets/lol/riot_static/15.10.1/img/perk-images/Styles/Domination/UltimateHunter/UltimateHunter.png" }
+            ]
+        ]
+    },
+    sorcery: {
+        keystones: [
+            { nombre: "Invocación: Aery", img: "https://static.bigbrain.gg/assets/lol/riot_static/15.10.1/img/perk-images/Styles/Sorcery/SummonAery/SummonAery.png" },
+            { nombre: "Cometa Arcano", img: "https://static.bigbrain.gg/assets/lol/riot_static/15.10.1/img/perk-images/Styles/Sorcery/ArcaneComet/ArcaneComet.png" },
+            { nombre: "Fase Veloz", img: "https://static.bigbrain.gg/assets/lol/riot_static/15.10.1/img/perk-images/Styles/Sorcery/PhaseRush/PhaseRush.png" }
+        ],
+        slots: [
+            [
+                { nombre: "Arcanista Axiomático", img: "https://static.bigbrain.gg/assets/lol/riot_static/15.10.1/img/small-perk-images/Styles/Sorcery/NullifyingOrb/Axiom_Arcanist.png" },
+                { nombre: "Anillo de Flujo de Maná", img: "https://static.bigbrain.gg/assets/lol/riot_static/15.10.1/img/perk-images/Styles/Sorcery/ManaflowBand/ManaflowBand.png" },
+                { nombre: "Capa del Nimbo", img: "https://static.bigbrain.gg/assets/lol/riot_static/15.10.1/img/small-perk-images/Styles/Sorcery/NimbusCloak/6361.png" }
+            ],
+            [
+                { nombre: "Trascendencia", img: "https://static.bigbrain.gg/assets/lol/riot_static/15.10.1/img/perk-images/Styles/Sorcery/Transcendence/Transcendence.png" },
+                { nombre: "Celeridad", img: "https://static.bigbrain.gg/assets/lol/riot_static/15.10.1/img/small-perk-images/Styles/Sorcery/Celerity/CelerityTemp.png" },
+                { nombre: "Concentración Absoluta", img: "https://static.bigbrain.gg/assets/lol/riot_static/15.10.1/img/perk-images/Styles/Sorcery/AbsoluteFocus/AbsoluteFocus.png" }
+            ],
+            [
+                { nombre: "Quemadura", img: "https://static.bigbrain.gg/assets/lol/riot_static/15.10.1/img/perk-images/Styles/Sorcery/Scorch/Scorch.png" },
+                { nombre: "Caminata Sobre Agua", img: "https://static.bigbrain.gg/assets/lol/riot_static/15.10.1/img/perk-images/Styles/Sorcery/Waterwalking/Waterwalking.png" },
+                { nombre: "Tormenta Creciente", img: "https://static.bigbrain.gg/assets/lol/riot_static/15.10.1/img/perk-images/Styles/Sorcery/GatheringStorm/GatheringStorm.png" }
+            ]
+        ]
+    },
+    resolve: {
+        keystones: [
+            { nombre: "Agarre del Perpetuo", img: "https://static.bigbrain.gg/assets/lol/riot_static/15.10.1/img/perk-images/Styles/Resolve/GraspOfTheUndying/GraspOfTheUndying.png" },
+            { nombre: "Réplica", img: "https://static.bigbrain.gg/assets/lol/riot_static/15.10.1/img/perk-images/Styles/Resolve/VeteranAftershock/VeteranAftershock.png" },
+            { nombre: "Guardián", img: "https://static.bigbrain.gg/assets/lol/riot_static/15.10.1/img/perk-images/Styles/Resolve/Guardian/Guardian.png" }
+        ],
+        slots: [
+            [
+                { nombre: "Demolición", img: "https://static.bigbrain.gg/assets/lol/riot_static/15.10.1/img/perk-images/Styles/Resolve/Demolish/Demolish.png" },
+                { nombre: "Fuente de Vida", img: "https://static.bigbrain.gg/assets/lol/riot_static/15.10.1/img/perk-images/Styles/Resolve/FontOfLife/FontOfLife.png" },
+                { nombre: "Golpe de Escudo", img: "https://static.bigbrain.gg/assets/lol/riot_static/15.10.1/img/perk-images/Styles/Resolve/MirrorShell/MirrorShell.png" }
+            ],
+            [
+                { nombre: "Acondicionamiento", img: "https://static.bigbrain.gg/assets/lol/riot_static/15.10.1/img/perk-images/Styles/Resolve/Conditioning/Conditioning.png" },
+                { nombre: "Segundo Aire", img: "https://static.bigbrain.gg/assets/lol/riot_static/15.10.1/img/perk-images/Styles/Resolve/SecondWind/SecondWind.png" },
+                { nombre: "Coraza Ósea", img: "https://static.bigbrain.gg/assets/lol/riot_static/15.10.1/img/perk-images/Styles/Resolve/BonePlating/BonePlating.png" }
+            ],
+            [
+                { nombre: "Crecimiento Excesivo", img: "https://static.bigbrain.gg/assets/lol/riot_static/15.10.1/img/perk-images/Styles/Resolve/Overgrowth/Overgrowth.png" },
+                { nombre: "Revitalizar", img: "https://static.bigbrain.gg/assets/lol/riot_static/15.10.1/img/perk-images/Styles/Resolve/Revitalize/Revitalize.png" },
+                { nombre: "Inquebrantable", img: "https://static.bigbrain.gg/assets/lol/riot_static/15.10.1/img/small-perk-images/Styles/Sorcery/Unflinching/Unflinching.png" }
+            ]
+        ]
+    },
+    inspiration: {
+        keystones: [
+            { nombre: "Aumento Glacial", img: "https://static.bigbrain.gg/assets/lol/riot_static/15.10.1/img/perk-images/Styles/Inspiration/GlacialAugment/GlacialAugment.png" },
+            { nombre: "Libro de Hechizos Abierto", img: "https://static.bigbrain.gg/assets/lol/riot_static/15.10.1/img/perk-images/Styles/Inspiration/UnsealedSpellbook/UnsealedSpellbook.png" },
+            { nombre: "Primer Golpe", img: "https://static.bigbrain.gg/assets/lol/riot_static/15.10.1/img/perk-images/Styles/Inspiration/FirstStrike/FirstStrike.png" }
+        ],
+        slots: [
+            [
+                { nombre: "Destello Hextech", img: "https://static.bigbrain.gg/assets/lol/riot_static/15.10.1/img/perk-images/Styles/Inspiration/HextechFlashtraption/HextechFlashtraption.png" },
+                { nombre: "Calzado Mágico", img: "https://static.bigbrain.gg/assets/lol/riot_static/15.10.1/img/small-perk-images/Styles/Inspiration/MagicalFootwear/MagicalFootwear.png" },
+                { nombre: "Reembolso", img: "https://static.bigbrain.gg/assets/lol/riot_static/15.10.1/img/small-perk-images/Styles/Inspiration/CashBack/CashBack2.png" }
+            ],
+            [
+                { nombre: "Tónico Triple", img: "https://static.bigbrain.gg/assets/lol/riot_static/15.10.1/img/small-perk-images/Styles/Inspiration/PerfectTiming/AlchemistCabinet.png" },
+                { nombre: "Tónico de Distorsión Temporal", img: "https://static.bigbrain.gg/assets/lol/riot_static/15.10.1/img/perk-images/Styles/Inspiration/TimeWarpTonic/TimeWarpTonic.png" },
+                { nombre: "Entrega de Galletas", img: "https://static.bigbrain.gg/assets/lol/riot_static/15.10.1/img/perk-images/Styles/Inspiration/BiscuitDelivery/BiscuitDelivery.png" }
+            ],
+            [
+                { nombre: "Perspicacia Cósmica", img: "https://static.bigbrain.gg/assets/lol/riot_static/15.10.1/img/perk-images/Styles/Inspiration/CosmicInsight/CosmicInsight.png" },
+                { nombre: "Velocidad de Aproximación", img: "https://static.bigbrain.gg/assets/lol/riot_static/15.10.1/img/small-perk-images/Styles/Resolve/ApproachVelocity/ApproachVelocity.png" }, 
+                { nombre: "Comodín", img: "https://static.bigbrain.gg/assets/lol/riot_static/15.10.1/img/small-perk-images/Styles/Inspiration/JackOfAllTrades/JackofAllTrades2.png" }
+            ]
+        ]
+    }
+};
+
+let rutaPrimariaSelec = 'precision';
+let rutaSecundariaSelec = 'domination';
+let runasSecundariasSelec = [];
+
+//
+
+//
+
